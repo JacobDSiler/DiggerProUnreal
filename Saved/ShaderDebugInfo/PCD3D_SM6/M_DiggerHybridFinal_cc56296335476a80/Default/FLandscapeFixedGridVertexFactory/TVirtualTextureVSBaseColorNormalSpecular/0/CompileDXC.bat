03cf9bffeb20220cb65f762d9c14b23caff4ac4d
Jacob Siler <46026740+JacobDSiler@users.noreply.github.com>
2025-08-18 06:20:04 +0100

    Ignore Unreal Engine Saved directory

--- Saved/ShaderDebugInfo/PCD3D_SM6/M_DiggerHybridFinal_cc56296335476a80/Default/FLandscapeFixedGridVertexFactory/TVirtualTextureVSBaseColorNormalSpecular/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="C:\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% /auto-binding-space 0 /Zpr /O3 -Wno-parentheses-equality -disable-lifetime-markers /T vs_6_6 /E MainVS /Fc VirtualTextureMaterial.d3dasm /Fo VirtualTextureMaterial.dxil VirtualTextureMaterial.usf
-:END
-PAUSE
